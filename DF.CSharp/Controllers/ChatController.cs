e3d2fe7ea3cc10fa7f769c5efede979f94993031
Julián Podbersich <julian.podbersich@valkimia.com>
2024-08-14 10:15:02 -0300

    Initial Commit

---
+++ DF.CSharp/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using DF.CSharp.Helpers;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Dialogflow.v2beta1;
+using Google.Apis.Dialogflow.v2beta1.Data;
+using Google.Apis.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DF.CSharp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpGet("GetChatAsync")]
+        public dynamic Chat(string message)
+        {
+            string currentSession = Guid.NewGuid().ToString();
+            GoogleCredential? credential = new GoogleHelper(_configuration).CredentialScope();
+            string projectId = _configuration["Google:ProjectId"]!;
+
+            var response = new DialogflowService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = projectId
+            }).Projects.Agent.Sessions.DetectIntent(
+                new GoogleCloudDialogflowV2beta1DetectIntentRequest
+                {
+                    QueryInput = new GoogleCloudDialogflowV2beta1QueryInput
+                    {
+                        Text = new GoogleCloudDialogflowV2beta1TextInput
+                        {
+                            Text = message,
+                            LanguageCode = "en-US"
+                        }
+                    }
+                },
+                "projects/" + projectId + "/agent/sessions/" + currentSession).Execute();
+            dynamic queryResult = response.QueryResult;
+            return queryResult;
+        }
+    }
+}
---
+++ DF.CSharp/Helpers/GoogleHelper.cs
@@ -0,0 +1,33 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Dialogflow.v2beta1;
+
+namespace DF.CSharp.Helpers
+{
+    public class GoogleHelper(IConfiguration configuration)
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public GoogleCredential? CredentialScope()
+        {
+            GoogleCredential credential;
+            string currentDirectory = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(currentDirectory, _configuration["Google:ConfigPath"]!);
+
+            try
+            {
+                using (Stream configStream = new FileStream(configPath, FileMode.Open))
+                {
+                    credential = GoogleCredential.FromStream(configStream);
+                }
+
+                var scope = credential.CreateScoped(DialogflowService.Scope.CloudPlatform);
+
+                return scope;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
